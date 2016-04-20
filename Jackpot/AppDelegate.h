12d0aa503c81ddd4fcff8ac9906d0acda0f006ad
Donny Davis <donnywdavis@icloud.com>
2016-04-20 09:30:58 -0400

    Initial Commit

---
+++ Jackpot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jackpot
+//
+//  Created by Donny Davis on 4/20/16.
+//  Copyright © 2016 Donny Davis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jackpot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jackpot
+//
+//  Created by Donny Davis on 4/20/16.
+//  Copyright © 2016 Donny Davis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
